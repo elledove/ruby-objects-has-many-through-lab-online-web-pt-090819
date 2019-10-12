886d83dd7fcc9550eaa118a0ae6d4e0a88d7a838
Lauren Dove <elledoveinc@gmail.com>
2019-10-12 20:55:45 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,8 @@
 require 'pry'
-class Appointment
+# class Appointment
   
-  attr_accessor :date, :patient, :doctor
-  @@all = [ ]
+#   attr_accessor :date, :patient, :doctor
+#   @@all = [ ]
   
   # def initialize(date, patient, doctor)
   #   @date = date
